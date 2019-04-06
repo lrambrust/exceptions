87e21b8d4f71eed276a17b95ed673daa626138b6
Lucas Ambrust <ambrust.lucas@gmail.com>
2019-04-06 15:14:09 -0300

    Projeto finalizado

---
+++ Exception/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exception.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException (string message) : base(message)
+        {
+        }
+    }
+}
